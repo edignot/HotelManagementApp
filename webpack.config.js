69e272d76d19b674432d830e511188e64a25007d
Ed Ignot <edignot@gmail.com>
2020-04-16 11:05:51 -0600

    Multiple Html files

--- webpack.config.js
+++ webpack.config.js
@@ -11,33 +11,47 @@ module.exports = {
   mode: 'development',
   // CSS and file (image) loaders
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'images/',
-                  publicPath: 'images/'
-                }
-              }
-            ]
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            publicPath: 'images/'
+          }
+        }]
       }
     ],
   },
   // Below is needed for webpack-dev-server
   plugins: [
     new HtmlWebpackPlugin({
+      filename: 'index.html',
       template: './src/index.html'
-    })
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'admin-login.html',
+      template: './src/admin-login.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'user-login.html',
+      template: './src/user-login.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'user.html',
+      template: './src/user.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'admin.html',
+      template: './src/admin.html'
+    }),
   ],
   devServer: {
-         contentBase: './dist'
+    contentBase: './dist'
   }
 };
