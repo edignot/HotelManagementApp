38381bb71d1c9be9ba72a9bc78814fbc7ddb4128
Ed Ignot <edignot@gmail.com>
2020-04-17 21:34:56 -0600

    Hotel tests setup

--- test/hotel-test.js
+++ test/hotel-test.js
@@ -1,8 +1,15 @@
 import chai from 'chai';
 const expect = chai.expect;
+import Hotel from '../src/hotel';
+import bookingsTestData from '../test-data/bookings-test-data.js';
+import roomsTestData from '../test-data/rooms-test-data.js';
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
+describe('See if the tests are running', function () {
+  let hotel;
+  beforeEach(() => {
+    hotel = new Hotel(roomsTestData, bookingsTestData);
+  });
+  it('should return true', function () {
     expect(true).to.equal(true);
   });
 });
